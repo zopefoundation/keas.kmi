fe1c7669ed65c9b6e7f702c50fe5447926d91ed3
Marius Gedminas <marius@gedmin.as>
2008-09-04 23:58:36 +0000

    A simple KeyHolder utility is available in keas.kmi.keyholder.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(*rnames):
 
 setup (
     name='keas.kmi',
-    version='0.2.1dev',
+    version='0.3.0dev',
     author = "Stephan Richter and the Zope Community",
     author_email = "zope-dev@zope.org",
     description = "A Key Management Infrastructure",
