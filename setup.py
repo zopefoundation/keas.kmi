016faa8498ac9b692bcd1c79a065e36e14fc58db
Stephan Richter <stephan.richter@gmail.com>
2008-09-04 07:44:50 +0000

    version bump.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(*rnames):
 
 setup (
     name='keas.kmi',
-    version='0.1.0',
+    version='0.2.0dev',
     author = "Stephan Richter and the Zope Community",
     author_email = "zope-dev@zope.org",
     description = "A Key Management Infrastructure",
