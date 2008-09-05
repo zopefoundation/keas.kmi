a5d69c9ee892801064c6d8d3211817a9d41110da
Marius Gedminas <marius@gedmin.as>
2008-09-05 00:00:05 +0000

    Prepare to release keas.kmi 0.3.0.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(*rnames):
 
 setup (
     name='keas.kmi',
-    version='0.3.0dev',
+    version='0.3.0',
     author = "Stephan Richter and the Zope Community",
     author_email = "zope-dev@zope.org",
     description = "A Key Management Infrastructure",
