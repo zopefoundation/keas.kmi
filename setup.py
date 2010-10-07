e6e3fb9e4204d07038bd2025c9a5b5ff7d9c5278
Stephan Richter <stephan.richter@gmail.com>
2010-10-07 20:14:03 +0000

    vb

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(*rnames):
 
 setup (
     name='keas.kmi',
-    version = '2.1.0',
+    version = '2.1.1dev',
     author = "Stephan Richter and the Zope Community",
     author_email = "zope-dev@zope.org",
     description = "A Key Management Infrastructure",
