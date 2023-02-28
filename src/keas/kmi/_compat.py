c4556a037f37dde41d7060bcdefff62da9164a22
Michael Howitz <mh@gocept.com>
2023-02-28 08:25:14 +0100

    Config with pure python template f4c150cf (#12)
    * Bumped version for breaking release.
    
    * Drop support for Python 2.7, 3.5, 3.6.
    
    * Add support for Python 3.10, 3.11.

--- src/keas/kmi/_compat.py
+++ src/keas/kmi/_compat.py
@@ -11,48 +11,26 @@
 # FOR A PARTICULAR PURPOSE
 #
 ##############################################################################
-from six import PY3
-
 # This code was copied from ZODB/_compat.py
 
-if not PY3:
-    # Python 2.x
-    # PyPy's cPickle doesn't have noload, and noload is broken in Python 2.7,
-    # so we need zodbpickle.
-    # Get the fastest working version we can (PyPy has no fastpickle)
-    try:
-        import zodbpickle.fastpickle as cPickle
-    except ImportError:
-        import zodbpickle.pickle as cPickle
-    Pickler = cPickle.Pickler
-    Unpickler = cPickle.Unpickler
-else:
-    # Python 3.x: can't use stdlib's pickle because
-    # http://bugs.python.org/issue6784
-    import zodbpickle.pickle
-
-    class Pickler(zodbpickle.pickle.Pickler):
-        def __init__(self, f, protocol=None):
-            super(Pickler, self).__init__(f, protocol)
-
-    class Unpickler(zodbpickle.pickle.Unpickler):
-        def __init__(self, f):
-            super(Unpickler, self).__init__(f)
-
-        # Py3: Python 3 doesn't allow assignments to find_global,
-        # instead, find_class can be overridden
-
-        find_global = None
-
-        def find_class(self, modulename, name):
-            if self.find_global is None:
-                return super(Unpickler, self).find_class(modulename, name)
-            return self.find_global(modulename, name)
+import zodbpickle.pickle
 
 
-try:
-    # XXX: why not just import BytesIO from io?
-    from cStringIO import StringIO as BytesIO
-except ImportError:
-    # Python 3.x
-    from io import BytesIO  # noqa: F401 'io.BytesIO' imported but unused
+class Pickler(zodbpickle.pickle.Pickler):
+    def __init__(self, f, protocol=None):
+        super().__init__(f, protocol)
+
+
+class Unpickler(zodbpickle.pickle.Unpickler):
+    def __init__(self, f):
+        super().__init__(f)
+
+    # Python doesn't allow assignments to find_global,
+    # instead, find_class can be overridden
+
+    find_global = None
+
+    def find_class(self, modulename, name):
+        if self.find_global is None:
+            return super().find_class(modulename, name)
+        return self.find_global(modulename, name)
